b453e58ca792c256cea2435d1108424a542a4e21
Labos_NiñoB. <ninolabos02@gmail.com>
2025-08-13 18:45:43 +0800

    Main

---
+++ CounterApp.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+
+const CounterApp = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <View>
+      <Text>Counter App</Text>
+      <Text>{count}</Text>
+      <Button title="Increment" onPress={() => setCount(count + 1)} />
+      <Button title="Decrement" onPress={() => setCount(count - 1)} />
+    </View>
+  );
+};
+
+export default CounterApp;
